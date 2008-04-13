4b0915e8e8a270a29a353d1b834aba2cd4d12c0d
kioopi <kioopi@c936e63d-854a-0410-95b1-3b1919220361>
2008-04-13 18:59:57 +0000

    Refactoring. Extracted DatesManager into a managers file.

--- apps/dates/models.py
+++ apps/dates/models.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 from geopy import geocoders  
 
 from plotter import settings
+from plotter.apps.dates.managers import DateManager
 
 
 DATE_CATEGORIES = {
@@ -22,65 +23,6 @@ COUNTRY_CHOICES = (
  ( 'de' , 'de'),
 )
 
-# TODO this has to go in its own file 
-
-class DateManager(models.Manager):
-    """A Custom Manager for Dates""" 
-    def published(self):
-        """Returns a queryset of all published dates"""
-        return self.filter(publish=True) 
-
-    def oneday(self, startdate): 
-        """returns a queryset with all dates on the passed datetimes day"""
-        return self.published().filter(
-              startdate__year=startdate.year,
-              startdate__month=startdate.month,
-              startdate__day=startdate.day
-                                      ) 
-
-    def today(self): 
-        """returns a queryset of all published dates today"""
-        return self.oneday(datetime.datetime.today()) 
-
-    def by_place( self, place ):  
-        (country, zipcode) = place.split('-')  
-        return self.published().filter(adress__country__iexact=country, adress__zipcode__istartswith=zipcode)
-
-    def by_date_and_place(self, date, place):
-        (country, zipcode) = place.split('-')  
-        return self.by_date(date).filter(adress__country__iexact=country, adress__zipcode__istartswith=zipcode) 
-        
-
-    def by_date(self, date):
-        """Returns a queryset of dates matching a date"""
-         
-        # TODO handle the case if date is a datetime-object
-
-        # first try if date matches a set of strings 
-        if date.lower() == 'all': # all dates
-            return self.published()
-        if date.lower() == 'today': # dates for today 
-            return self.today()
-        if date.lower() == 'tomorrow':  # date for manana
-            return self.oneday(datetime.today()+timedelta(days=+1))
-
-        # second try if date is a minus(-)-seperated date
-        qs = self.published() 
-        parts = date.split('-') 
-        if len(parts)>=1: # dates for a certain year
-            qs = qs.filter(startdate__year=int(parts[0])) 
-           
-        if len(parts)>=2: # dates for a certain year and month
-            qs = qs.filter(startdate__month=int(parts[1])) 
-           
-        if len(parts)==3: # dates for a certain day
-            qs = qs.filter(startdate__day=int(parts[2])) 
-        return qs
-
-        # not a match
-        #  TODO log the value of date, for future optimization and
-        #  TODO return an empty queryset, i guess 
- 
 class Organizer(models.Model): 
     name = models.CharField(max_length=100)
     slug = models.SlugField(prepopulate_from=('name', ))
