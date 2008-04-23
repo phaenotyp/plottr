6f7cc9302dbea3983f2d2ee341b33ab7de09a01e
kioopi <kioopi@c936e63d-854a-0410-95b1-3b1919220361>
2008-04-23 22:01:00 +0000

    Major renewal of the restful-api url layout.

--- trunk/apps/dates/urls.py
+++ trunk/apps/dates/urls.py
@@ -17,7 +17,17 @@ urlpatterns = patterns('plotter.apps.dates.views',
     # dates filtered by date, country & zipcode,  shortened zipcodes should be treated as wildcards
     # plottr.de/dates/2008-07/de-50672/
     # plottr.de/dates/today/de-50/
-    (r'(?P<date>[-\w]+)/(?P<country>[a-z]{2})-(?P<zip>\d{1,5})/$', 'by_place'),
+#    (r'(?P<date>[-\w]+)/(?P<country>[a-z]{2})-(?P<zip>\d{1,5})/$', 'by_place'),
+
+    # dates filtered by date, country & zipcode,  shortened zipcodes should be treated as wildcards
+    # plottr.de/dates/2008-07/de/50672/
+    # plottr.de/dates/today/de/50/
+    (r'(?P<date>[-\w]+)/(?P<country>[a-z]{2})/(?P<zipcode>\d{1,5})/$', 'by_place'),
+
+    # same as above but with several zips
+    #plottr.de/dates/2008-07/de/50672,50675,50676/
+                                              # this grouping could be problematic
+    (r'(?P<date>[-\w]+)/(?P<country>[a-z]{2})/(?P<zipcode>\d{5}(,\d{5})+)/$', 'by_place'),
 
  
     # date filtered by date and geocoordinates and distance 
@@ -25,14 +35,16 @@ urlpatterns = patterns('plotter.apps.dates.views',
     # plottr.de/dates/tommorrow/1.231233,2.34234/5km/   
     (r'(?P<date>[-\w]+)/(?P<geo>[THISNEEDSAREGEX])/(?P<distance>\w+)/$', 'by_geo'),
 
-
+    # one single date
+    # plottr.de/dates/1234/
+    url(r'id/(?P<id>\d+)/$', 'single_by_id', name='single_by_id'),
 
     # one single date
     # plottr.de/dates/2008-04-14/click-clack-club/     
-    url(r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
-       'single',
-       name='single'
-    ),
+#    url(r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
+ #      'single',
+#       name='single'
+#    ),
 
     # dates filtered by date 
     # plottr.de/dates/2008-04-14/
--- trunk/apps/dates/views.py
+++ trunk/apps/dates/views.py
@@ -8,21 +8,33 @@ from plotter.utils import json_encode
 from datetime import datetime, timedelta
 
 
-def single(request, year, month, day, slug): 
-    """One single date.
-       /dates/2008-03-17/domingo-club/
-    """
-    date = get_object_or_404(Date.objects.by_date('%s-%s-%s'%(year,month,day)),slug=slug) 
+def single_response(request, date):
+    """Takes a single date and generates a response for it based on accept header"""
+    if request.accepts('application/ical'):
+        return  HttpResponse(date.as_ical(), mimetype='application/ical')
 
-    if request.accepts('application/ical'):    
-        return  HttpResponse(date.as_ical(), mimetype='application/ical') 
-    
-    if request.accepts('application/json'):    
-        return  HttpResponse(date.as_json(), mimetype='application/json') 
+    if request.accepts('application/json'):
+        return  HttpResponse(date.as_json(), mimetype='application/json')
 
     if request.accepts('text/html'):
         return render_to_response('dates/single.html', {'object': date})
-   
+
+def single(request, year, month, day, country, zipcode, slug):
+    """One single date.
+       /dates/2008-03-17/domingo-club/
+    """
+    #TODO work country and zipcode into the queryset
+    date = get_object_or_404(Date.objects.by_date('%s-%s-%s'%(year,month,day)),slug=slug)
+    return single_response(request, date)
+
+def single_by_id(request, id):
+    """One single date.
+       /dates/2345/
+    """
+    date = get_object_or_404(Date.objects.all(),pk=id)
+    return single_response(request, date)
+
+  
 
 def date_list(request):
     """ a list of dates
@@ -70,7 +82,46 @@ def by_date(request, date):
            template_object_name='dates'
         )     
 
-def by_place(request, date, country, zip=None):
+def by_place(request, date, country, zipcode=None):
+    """ a list of dates, matching the parameter date and place
+        /dates/2009-12-23/de-52123/
+    """
+
+    # get dates by dates and country
+    dates = Date.objects.by_date(date).filter(adress__country__iexact=country)
+    # filter the queryset by zipcode
+    if zipcode:
+        # if several zipcodes are passed, seperated by commata
+        if ',' in zipcode:
+           zips = zipcode.split(',')
+           dates = dates.filter(adress__zipcode__in=zips)
+        # if a single or partial zipcode is passed
+        else:
+           dates = dates.filter(adress__zipcode__istartswith=str(zipcode))
+
+    if request.accepts('application/json'):
+        # denormalization to avoid an abundance of requests.
+        # ForeignKey-Fields get serialized only by their id
+        # so they are copied to be serialized like a normal object.
+        # TODO: move the denormalization into the model.
+        for d in dates:
+            d.absolute_url = d.get_absolute_url()
+            d.adressdata = d.adress
+            d.locationdata = d.location
+        return HttpResponse(json_encode(dates), mimetype='application/json')
+
+    if request.accepts('text/html'):
+        # using the generic view with custom parameters
+        return object_list(
+           request,
+           queryset = dates,
+           template_name='dates/list.html',
+           template_object_name='dates'
+        )
+
+
+
+def _by_place(request, date, country, zip=None):
     """ a list of dates, matching the parameter date and place
         /dates/2009-12-23/de-52123/
     """
